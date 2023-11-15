ebc8b5afd96251d44ce6abc9a25b08a45082d24a
Brunouch <brunogabrielzordenunes@gmail.com>
2023-11-15 20:31:26 -0300

    First Steps in Java with Spring Boot 3

---
+++ rest-spring-boot-java-erudio/src/main/java/bruno/spring/java/Startup.java
@@ -0,0 +1,13 @@
+package bruno.spring.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Startup {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Startup.class, args);
+	}
+
+}
