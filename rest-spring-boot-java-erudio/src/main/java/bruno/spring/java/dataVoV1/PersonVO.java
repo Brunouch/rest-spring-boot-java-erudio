25c19d5bda5cf82372e561e698c95a850826b479
Brunouch <brunogabrielzordenunes@gmail.com>
2024-04-01 22:16:49 -0300

    Deixando a API RestFull com o HATEOAS e utilizando o JUnit para tests da classe PersonService.

--- rest-spring-boot-java-erudio/src/main/java/bruno/spring/java/dataVoV1/PersonVO.java
+++ rest-spring-boot-java-erudio/src/main/java/bruno/spring/java/dataVoV1/PersonVO.java
@@ -2,20 +2,26 @@ package bruno.spring.java.dataVoV1;
 
 import java.io.Serializable;
 
-import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import org.springframework.hateoas.RepresentationModel;
 
-import jakarta.persistence.Column;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.github.dozermapper.core.Mapping;
+
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
-public class PersonVO implements Serializable {
+@JsonPropertyOrder({"id", "firstName", "lastName", "address", "gender"})
+public class PersonVO extends RepresentationModel<PersonVO> implements Serializable {
     private static final long serialVersionUID = 1L;
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
 
-    private long id;
+    @Mapping("id")
+    @JsonProperty("id")
+    private long key;
     private String firstName;
     private String lastName;
     private String address;
@@ -24,15 +30,14 @@ public class PersonVO implements Serializable {
 
     public PersonVO() {}
 
-
-    public long getId() {
-        return id;
+    public long getKey() {
+        return key;
     }
 
-
-    public void setId(long id) {
-        this.id = id;
+    public void setKey(long key) {
+        this.key = key;
     }
+    
 
 
     public String getFirstName() {
@@ -78,7 +83,7 @@ public class PersonVO implements Serializable {
     public int hashCode() {
         final int prime = 31;
         int result = 1;
-        result = prime * result + (int) (id ^ (id >>> 32));
+        result = prime * result + (int) (key ^ (key >>> 32));
         result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
         result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
         result = prime * result + ((address == null) ? 0 : address.hashCode());
@@ -95,7 +100,7 @@ public class PersonVO implements Serializable {
         if (getClass() != obj.getClass())
             return false;
         PersonVO other = (PersonVO) obj;
-        if (id != other.id)
+        if (key != other.key)
             return false;
         if (firstName == null) {
             if (other.firstName != null)
